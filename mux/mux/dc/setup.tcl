9d953f05d2d97fd742dbf8c74b7ddb8a54ed466e
ThunderCarver <27229985+ThunderCarver@users.noreply.github.com>
2022-05-15 08:13:56 +0800

    Add files via upload

---
+++ mux/mux/dc/setup.tcl
@@ -0,0 +1,5 @@
+
+set search_path "/home/lishang/ckfang/Projects/crossbar_tcl"
+set link_library "/home/lishang/ckfang/Projects/crossbar_tcl/pdk/smic180/digital/sc/synopsys/typical.db"
+set target_library "/home/lishang/ckfang/Projects/crossbar_tcl/pdk/smic180/digital/sc/synopsys/typical.db"
+set symbol_library "/home/lishang/ckfang/Projects/crossbar_tcl/pdk/smic180/digital/sc/synopsys/smic18.sdb"
